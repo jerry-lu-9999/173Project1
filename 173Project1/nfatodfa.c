bd61f4153c87c052a4bf65762d690adea55b2dd3
ykang1001 <y.kang@rochester.edu>
2019-02-06 19:42:35 -0500

    nfa to dfa psedo code

--- 173Project1/nfatodfa.c
+++ 173Project1/nfatodfa.c
@@ -3,37 +3,10 @@
 #include <stdbool.h>
 #include "IntHashSet.h"
 #include "LinkedList.h"
-/*
-typedef struct Node* Node;
 
-struct Node
-{
-    IntHashSet set;
-    Node next;
-    Node prev;
-};
-
-struct LinkedList {
-    Node first;
-    Node last;
-}
-
-LinkedList new_LinkedList() {
-	LinkedList this = (LinkedList)malloc(sizeof(struct LinkedList));
-	this->first = this->last = NULL;
-	return this;
-}
-
-static Node new_Node(I) {
-	Node this = (Node)malloc(sizeof(struct Node));
-	this->data = data;
-	this->next = this->prev = NULL;
-	return this;
-}
-*/
 LinkedList LinkedList_add_set(LinkedList list, IntHashSet set){
     if(!LinkedList_contains_set(list, set)){
-        LinkedList_add_at_end(list, set);
+        LinkedList_add_at_end(list, (void*) set);
     }
     return list;
 }
@@ -41,22 +14,105 @@ LinkedList LinkedList_add_set(LinkedList list, IntHashSet set){
 bool LinkedList_contains_set(LinkedList list, IntHashSet set){
     LinkedListIterator iterate = LinkedList_iterator(list);
     while(LinkedListIterator_hasNext(iterate)){
-        if(IntHashSet_equals(set, LinkedListIterator_next(iterate))){
+        if(IntHashSet_equals(set, (IntHashSet*) LinkedListIterator_next(iterate))){
             return true;
         }
     }
     return false;
 }
 
+
+
 void nfatodfa (NFA nfa) {
-    LinkedList allSets = new_LinkedList();
+    LinkedList allStates = new_LinkedList();
+    LinkedList processingStates = new_LinkedList();
+    LinkedList_add_at_front(allStates, 0);
+    LinkedList_add_at_front(processingStates, 0);
+ 
+    IntHashSet cur = new_IntHashSet(20);
+    IntHashSet newCur = new_IntHashSet(20);
+    IntHashSet tempCur = new_IntHashSet(20);
+
+    char inputSymb[36] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};  
+
+
+    while(!LinkedList_isEmpty(processingStates)){
+        IntHashSet_insert(cur, LinkedList_pop(processingStates));
+
+        for(int i = 0; i < 36; i++){
+            
+            // newCurr = get_transition2(nfa old, curr, c);
+            // if(new_curr is not in allStates) {
+            //     put new_curr in allStates;
+            //     put new_Curr in states to process;
+            // }
+            
+            // i = index of curr in allState
+            // destination = index of newCurr in allState 
+
+            // set_transition(dfa new, i, char, destination);
+
+        }
+
+    }
+
+
+    // LinkedList allSets = new_LinkedList();
+    // IntHashSet curStates = new_IntHashSet(20);
+    // IntHashSet newCur = new_IntHashSet(20);
+    // IntHashSet tempCur = new_IntHashSet(20);
+    // LinkedList_add_at_front(allSets, curStates);
+    // LinkedListIterator iterate = LinkedList_iterator(allSets);
+    // while(LinkedListIterator_hasNext(iterate)){
+    //     LinkedListIterator iterate2 = LinkedList_iterator(curStates);
+    //     // for loop outside
+    //     // get new cur from transition table
+    //     // if no duplicates, add new cur to LinkedList
+    //     while(LinkedListIterator_hasNext(iterate2)){
+
+    //         for(int i = 0; i < NFA_INPUT; i++){
+    //             // get the set of states from cur and all input symbols
+    //             // tempCur = nfa->transitionTable[LinkedList_iterator(iterate2)][i];
+    //         }
+    //         // union temp cur with new cur
+            
+    //     }
+        
+    //     // cur = new cur
+
+    //     }
+    }
+
+
+/*
+
+LinkedList allStates, states to be processed;
+put state 0 into both;
+
+while (states to be processed is not empty) {
+
+    pop out the first element in the states to process
+    cur = popped out element
+    
+    for (c in Sigma){
+
+        new_curr = get_transition2(nfa old, curr, c);
+        if(new_curr is not in allStates) {
+            put new_curr in allStates;
+            put new_Curr in states to process;
+        }
+        i = index of curr in allState
+        destination = index of newCurr in allState 
+
+        set_transition(dfa new, i, char, destination);
+        
+    }
+
 
 }
 
-int main() {
-    LinkedList new_LinkedList();
-}
 
+*/
 
 
 
